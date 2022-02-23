58b020adb01800f612c6f04a8b881c907be65431
Jaideep <guntupallijaideep@gmail.com>
2022-02-23 17:42:52 +0530

    updated

--- static-page/src/index.js
+++ static-page/src/index.js
@@ -6,17 +6,6 @@ import Footer from "./Footer";
 import Heading from "./Heading";
 import List from "./List";
 
-/**
-Challenge: 
-
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-- Using flexbox, line up the nav items horizontally, and
-  put them inline with the React logo.
-- Change the image styling to happen in CSS instead of in-line
-  For practice, add a new class to the image in order to style it
-*/
-
 function Page() {
     return (
         <div className="relative h-screen">
