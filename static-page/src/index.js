eee3ef06d2ab584c70a5482d3dc6c0ac5c463c65
Jaideep <guntupallijaideep@gmail.com>
2022-02-23 14:45:50 +0530

    able to divide all components to chldren

--- static-page/src/index.js
+++ static-page/src/index.js
@@ -1,33 +1,69 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 /**
 Challenge: 
 
-Part 1: Create a page of your own using a custom Page component
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
 
-It should return an ordered list with the reasons why you're
-excited to be learning React :)
+ */
 
-Render your list to the page
-
-*/
-
-function Page() {
-  return (
-    <div className='flex flex-col gap-4 m-10'>
-      <img src='./logo192.png' alt='react-logo' className='w-20' />
-      <h1 className='font-bold text-4xl'>Fun Facts about React</h1>
-        <ul className='flex flex-col gap-4 list-disc list-inside'>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on Github</li>
-          <li>Is maintained by Facebook</li>
-          <li>Power thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-  );
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./logo192.png" alt="react logo" className="w-20" />
+            </nav>
+        </header>
+    );
 }
 
-ReactDOM.render(<Page />, document.querySelector("#root"));
+function Footer() {
+    return (
+        <footer className="pt-10 italic text-gray-400">
+            © 2022 Jaideep development. All rights reserved.
+        </footer>
+    );
+}
+
+function Heading() {
+    return (
+        <h1 className="font-bold text-3xl py-2">
+            Reasons I'm excited to learn React
+        </h1>
+    );
+}
+
+function List() {
+    return (
+        <ol className="list-disc list-inside">
+            <li>
+                It's a popular library, so I'll be able to fit in with the cool
+                kids!
+            </li>
+            <li>I'm more likely to get a job as a developer if I know React</li>
+        </ol>
+    );
+}
+
+function Page() {
+    return (
+        <div className="pl-10 pt-5">
+            <Header />
+            <Heading />
+            <List />
+            <Footer />
+        </div>
+    );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
